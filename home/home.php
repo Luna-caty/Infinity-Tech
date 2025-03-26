f198477b7d343f8c725d83eaf9bf8688e42f7c1c
Luna <lina.gali@yahoo.com>
2025-03-27 00:58:28 +0100

    starting home page just the nav bar

---
+++ home/home.php
@@ -0,0 +1,42 @@
+<?php
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Infnity-Tech</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="home.css">
+</head>
+
+<body>
+
+    <nav>
+        <div class="navbar">
+            <div class="logo">
+                <img src="../assets/logo.png" alt="logo">
+                <span>Infinity-Tech</span>
+
+            </div>
+            <ul class="nav-menu">
+                <li><a href="home.php">Home</a></li>
+                <li><a href="shop.php">Shop</a></li>
+                <li><a href="accessoires.php">Accessoires</a></li>
+                <li><a href="laptops.php">Laptops</a></li>
+            </ul>
+            <div class="nav_actions">
+                <img src="../assets/cart.png" alt="cart" class="cart-icon">
+                <a href="../register/signUp.php" class="register-btn">Register</a>
+            </div>
+        </div>
+    </nav>
+</body>
+
+</html
